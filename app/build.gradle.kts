1261f0938a95192be7a512686981a1c7ea7502dd
Javiehz <javier.encher@educa.jcyl.es>
2025-09-26 15:26:26 +0200

    add tests regarding save a new student.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,12 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    //UNIT TESTS
     testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+
+    //ANDROID TESTS
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
